b3d0395d9ecd09d4af972ba8cf2edab7162ad256
Rochyiat Ramdani <163083539+Rochyiat@users.noreply.github.com>
2024-06-28 15:04:39 +0700

    Revert "fitur edit"

--- routes/web.php
+++ routes/web.php
@@ -12,10 +12,4 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::resource('users', UserController::class);
-
-// Route::get('/users/{id}/edit', [UserController::class, 'edit'])->name('users.edit');
-// Route::put('/users/{id}', [UserController::class, 'update'])->name('users.update');
-
-// Route::get('/users/{id}/edit', 'UserController::class, 'edit'])->name('users.edit');
-// Route::put('/users/{id}', 'UserController@update')->name('users.update');
+Route::resource('user', UserController::class);
